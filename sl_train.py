25cebae326e6684981e29a462018960f1c1b1fa5
mariaalfaroc <mac77@alu.ua.es>
2024-02-26 19:29:06 +0100

    deterministic set to False (error raised by unavailability)

--- sl_train.py
+++ sl_train.py
@@ -18,7 +18,7 @@ torch.manual_seed(42)
 
 # Deterministic behavior
 torch.backends.cudnn.benchmark = False
-torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.deterministic = False
 
 from data.config import DS_CONFIG
 from my_utils.dataset import CTCDataset
@@ -155,7 +155,7 @@ def self_labelled_train(
         callbacks=callbacks,
         max_epochs=epochs,
         check_val_every_n_epoch=1,
-        deterministic=True,
+        deterministic=False,
         benchmark=False,
         precision="16-mixed",  # Mixed precision training
     )
